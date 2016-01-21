d59e6a08ad502796ed55e5c5667f75f2dd458f8a
riudomenc <david.aguade@gmail.com>
2016-01-21 16:05:56 +0100

    Update canopy.f90

--- ndf_modified_weibull_david_predawn/src/canopy.f90
+++ ndf_modified_weibull_david_predawn/src/canopy.f90
@@ -8,7 +8,6 @@
 ! Model version:                                          !
 ! ------------------------------------------------------- !
 
-!he afegit aquesta frase de prova a la línia 11 per a veure que passava. push des de l'ubuntu; prova per pull master
 module canopy
 
   !! >this module requires a summary< !!
